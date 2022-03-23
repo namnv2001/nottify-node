4ac0de542ee31e6f312fe71c85a1efd55788a261
Nam <namnv.forwork@gmail.com>
2022-03-23 16:17:15 +0700

    init

---
+++ routes/auth/index.js
@@ -0,0 +1,19 @@
+const User = require('../../models/User')
+
+const router = require('express').Router()
+
+router.post('/register', async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+  try {
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+module.exports = router
