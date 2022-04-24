d26fd667660753cf52052772da683d042191a158
namnv2001 <namnv.forwork@gmail.com>
2022-04-24 21:47:56 +0700

    add zing api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const userRouter = require('./src/routes/User')
 const authRouter = require('./src/routes/auth')
+const zingRouter = require('./src/routes/Zing')
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ app.use(express.json())
 
 app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter)
+app.use('/api/zing', zingRouter)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server is running on port 5000')
