b673de99bb6fd7bacfa5db60f0fc327e26e39bb1
namnv2001 <namnv.forwork@gmail.com>
2022-05-02 08:41:42 +0700

    update response for auth services

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -12,15 +12,21 @@ router.post('/register', async (req, res) => {
   })
   try {
     const user = await User.findOne({ username: req.body.username })
-    if (user) throw { status: 401, message: 'Username already exists!' }
+    if (user)
+      throw {
+        status: 401,
+        type: 'username',
+        message: 'Username already exists!',
+      }
     const userEmail = await User.findOne({ email: req.body.email })
-    if (userEmail) throw { status: 401, message: 'Email already exists!' }
+    if (userEmail)
+      throw { status: 401, type: 'email', message: 'Email already exists!' }
 
     const savedUser = await newUser.save()
     res.status(201).json(savedUser)
   } catch (error) {
     if (error.status === 401) {
-      return res.status(401).json(error.message)
+      res.status(401).json(error)
     } else res.status(400).json({ message: 'Bad request!' })
   }
 })
@@ -31,14 +37,15 @@ router.post('/login', async (req, res) => {
     const user = await User.findOne({
       username: req.body.username,
     })
-    if (!user) throw { status: 401, message: 'Wrong username!' }
+    if (!user)
+      throw { status: 401, type: 'username', message: 'Wrong username!' }
     const hashedPassword = CryptoJS.AES.decrypt(
       user.password,
       process.env.PASS_SEC,
     )
     const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
     if (originalPassword !== req.body.password)
-      throw { status: 401, message: 'Wrong password!' }
+      throw { status: 401, type: 'password', message: 'Wrong password!' }
 
     const accessToken = jwt.sign(
       {
@@ -51,9 +58,8 @@ router.post('/login', async (req, res) => {
     res.status(200).json({ ...others, accessToken })
   } catch (error) {
     if (error.status === 401) {
-      return res.status(401).json(error.message)
-    }
-    res.status(400).json({ message: 'Bad request!' })
+      res.status(401).json(error)
+    } else res.status(400).json({ message: 'Bad request!' })
   }
 })
 
